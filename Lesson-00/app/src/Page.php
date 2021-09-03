e86cb8e418d116cf7d8e1837ca4bd28f125a1d81
Vijay <vijay@smokeylemon.com>
2021-09-03 20:03:25 +1200

    new file

---
+++ Lesson-00/app/src/Page.php
@@ -0,0 +1,51 @@
+<?php
+
+use SilverStripe\CMS\Model\SiteTree;
+
+class Page extends SiteTree
+{
+    private static $db = array(
+    );
+
+    private static $has_one = array(
+    );
+}
+
+<?php
+
+use SilverStripe\CMS\Model\SiteTree;
+
+class Page extends SiteTree
+{
+    private static $db = array(
+    );
+
+    private static $has_one = array(
+    );
+}
+
+<?php
+
+use SilverStripe\CMS\Model\SiteTree;
+
+class Page extends SiteTree
+{
+    private static $db = array(
+    );
+
+    private static $has_one = array(
+    );
+}
+
+<?php
+
+use SilverStripe\CMS\Model\SiteTree;
+
+class Page extends SiteTree
+{
+    private static $db = array(
+    );
+
+    private static $has_one = array(
+    );
+}
